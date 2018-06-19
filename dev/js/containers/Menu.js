97f55463c7d3ea08487f10d6aa738c266834b1a4
Rishi Bhatia <rbhatia4245@gmail.com>
2018-06-19 14:57:47 +0530

    Add files via upload

---
+++ dev/js/containers/Menu.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+class Menu extends Component {
+	render(){
+		<div>
+		<ul>
+		<li key={} onClick={}><a href="#">HOME</a></li>
+		<li key={} onClick={}><a href="#">WORK</a></li>
+		<li key={} onClick={}><a href="#">ABOUT</a></li>
+		<li key={} onClick={}><a href="#">CONTACT</a></li>
+		</ul>
+		</div>
+	}
+}
+function mapStateToProps(state)
+{
+
+}
+export default connect(mapStateToProps)(header);
