ab56f58432e289b54df762e92b779e43de05e806
Rishi Bhatia <rbhatia4245@gmail.com>
2018-06-20 11:27:23 +0530

    Update Menu.js

--- dev/js/containers/Menu.js
+++ dev/js/containers/Menu.js
@@ -2,18 +2,21 @@ import React,{Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
-class Menu extends Component {
-	
+class Menu extends Component {	
 	render(){
 		if(!this.props.menudisplay)
 		{
-			return (
-					<div>
-					<h1>Hi my name is abhishek jassal</h1>
-					</div>
+			return(
+
+				<div>
+				<h1>hi my name is abhishek jassal</h1>
+				</div>
 				);
 		}
-		return(
+
+				
+
+				return(
 			<div>
 			<ul>
 			 <li key="home">Home</li>
