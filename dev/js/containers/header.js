f888b4b4d586835037fb1c38a7a07a42b84b697a
Rishi Bhatia <rbhatia4245@gmail.com>
2018-06-20 11:28:06 +0530

    Update header.js

--- dev/js/containers/header.js
+++ dev/js/containers/header.js
@@ -3,19 +3,24 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {selectMenu} from '../actions/menubutton';
 
-class header extends Component {
+class Header extends Component {
 	render(){
 		return(
 		<div>
 			<h1>Abhishek Jassal</h1>
-			<button onClick={() => this.props.selectMenu()} >MENU</button>
+			<button type="button" onClick={() => this.props.selectMenu()} >MENU</button>
 		</div>
 		);
 	}
 	
 }
+function mapStateToProps(state){
+	return{
+		menudisplay:state.menudisplay
+	}
+}
 function mapDispatchToProps(dispatch)
 {
 	return bindActionCreators({selectMenu:selectMenu},dispatch);
 }
-export default connect(mapDispatchToProps)(header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
